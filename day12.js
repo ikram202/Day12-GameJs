a762d0829409afdc48a67dc6c4c3bf3ffaa42b4a
LOUARNI IKRAM <116180576+ikram202@users.noreply.github.com>
2023-08-24 15:34:56 +0100

    Add files via upload

---
+++ day12.js
@@ -0,0 +1,39 @@
+
+function getComputerChoice() {
+    const choices = ['hajara', 'wara9a', 'mi9ass'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase(); 
+    if (playerSelection === computerSelection) {
+        return "It's tie!";
+    } else if (
+        (playerSelection === 'hajara' && computerSelection === 'mi9ass') ||
+        (playerSelection === 'mi9ass' && computerSelection === 'wara9a') ||
+        (playerSelection === 'wara9a' && computerSelection === 'hajara')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+console.log(playRound('hajara', getComputerChoice()));
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Enter your choice (hajara, wara9a, or mi9ass):");
+        const computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+    }
+}
+game();
+
+
+
+;
+
